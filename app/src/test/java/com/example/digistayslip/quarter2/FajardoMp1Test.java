8a81a147e1722bf7cb0849eb5fb90df77a6e3ab8
lucilaanthoneth-lang <lucilaanthoneth@gmail.com>
2026-09-16 15:56:33 +0800

    added my own java, in the com.example.digitalstayslip.quarter2 and write my code

---
+++ app/src/test/java/com/example/digistayslip/quarter2/FajardoMp1Test.java
@@ -0,0 +1,17 @@
+package com.example.digistayslip.quarter2;
+
+import org. junit.Test;
+public class FajardoMp1Test {
+    @Test
+    public void printMyProfile() {
+        String myName = "Lucila Fajardo";
+        String petName = "Minggay";
+        String favFood = "Kare-Kare";
+        int myAge =17;
+
+        System.out.println("----- MY DIGITAL PROFILE -----");
+        System.out.println("Hello, my name is " + myName + "and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName +".");
+        System.out.println("If I could, I would like to eat " + favFood+ "every single day!");
+    }
+}
--- app/src/test/java/com/example/digistayslip/quarter2/fajardo_mp1.java
+++
@@ -1,4 +0,0 @@
-package com.example.digistayslip.quarter2;
-
-public class fajardo_mp1 {
-}
