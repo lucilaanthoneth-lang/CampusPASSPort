3fba81081c88c507668269ef38b5d440b357de0d
JoLazaro-tech <eljolazaro146@gmail.com>
2026-09-16 14:56:27 +0800

    done

--- app/src/test/java/com/example/digistayslip/quarter2/lazaro.java
+++ app/src/test/java/com/example/digistayslip/quarter2/lazaro.java
@@ -1,4 +1,19 @@
 package com.example.digistayslip.quarter2;
-
+import org.junit.Test;
 public class lazaro {
+
+    @Test
+    public void printMYprofile() {
+        String myName = "Eljo";
+        String petname1 = "Creamy";
+        String petname2 = "Choco";
+        String favfood = "Adobo";
+        int myAge = 17;
+
+        System.out.println("___ MY DIGITAL PROFILE ___");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have 2 wonderful pets named" + petname1 + " and " + petname2 + ".");
+        System.out.println("if I could eat " + favfood + " everyday!");
+
+    }
 }
